953bd7ec913293c2650db91906ace6dad85cd9f9
Alys Scorpio <alyssad1182@gmail.com>
2021-08-03 15:41:36 -0700

    creating index routes

---
+++ public/routes/indexRoutes.js
@@ -0,0 +1,22 @@
+const data = require("../db/db.json")
+const { nanoid } = require('nanoid')
+
+module.exports = (app) => {
+    app.get("/api/notes", (req,res) => {
+        let newId = req.body;
+        newId.id = nanoid(2);
+        res.json(data);
+        console.log(data)
+    })
+
+    app.delete("/api/notes/:id", (req, res) => {
+        const chosen = req.params.id;
+
+        for (let i = 0; i < data.length; i++) {
+            if (chosen === data.id) {
+                res.json(chosen);
+            };
+        }
+    })
+};
+
