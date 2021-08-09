1350b522842d4801d88ebeaf57e662ac7a64629c
Alys Scorpio <alyssad1182@gmail.com>
2021-08-09 13:49:33 -0700

    finalizing

--- server.js
+++ server.js
@@ -1,48 +1,18 @@
 const express = require('express');
-const path = require('path');
-const fs = require('fs');
 
 const app = express();
+
+
 const PORT = process.env.PORT || 3001;
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'))
 
 
-// Route to get notes
-app.get('/api/notes', (req,res) => {
-  var notes;
-  fs.readFile('db/db.json', 'utf8', (error, data) => {
-    error ? console.error(error) : notes = JSON.parse(data);
-    console.log(notes);
-    res.json(notes);
-  });
-});
-
-// Route to post note
-app.post('/api/notes', (req, res) => {
-  var notes;
-  var note = req.body;
-  fs.readFile('db/db.json', 'utf8', (error, data) => {
-      error ? console.error(error) : notes =JSON.parse(data);
-      notes.push(note);
-      fs.writeFile('db/db.json', JSON.stringify(notes), (err) => 
-      err ? console.error(err) : res.json(notes));
-  });
-});
-
-// Route to delete note
-app.delete('api/notes/:id', (req, res) => {
-  var notes;
-  fs.readFile('db/db.json', 'utf8', (error,data) => {
-    error ? console.error(error) : notes = JSON.parse(data);
-    notes.splice(req.params.id, 1);
-    fs.writeFile('db/db.json', JSON.stringify(notes), (err) =>
-    err? console.error(err) : res.json(notes));
-  });
-});
+require('./routes/indexRoutes')(app);
+require('./routes/apiRoutes')(app); 
 
 
 app.listen(PORT, () => {
